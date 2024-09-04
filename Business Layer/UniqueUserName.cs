3028f686a91f124d4d48c2ee3dc84be455dca14e
loaisaber07 <loai.sabir@outlook.com>
2024-09-04 19:36:14 +0300

    update application user by add username and make it unique using custom validation called UniqueUserName

---
+++ Business Layer/UniqueUserName.cs
@@ -0,0 +1,29 @@
+﻿using Data_Access_Layer.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+    internal class UniqueUserName : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null) { 
+            var context = (ShippingDataBase)validationContext.GetService(typeof(ShippingDataBase));
+                var entity = context.Users.FirstOrDefault(s => s.UserName == value.ToString());
+                if (entity == null) {
+                    return new ValidationResult("User Name must be unique "); 
+                }       
+
+            
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
