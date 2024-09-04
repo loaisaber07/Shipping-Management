437a88e4740ce82b2fb00a13f92cbab482215fd7
Ahmed Kamel <ahmedkamel19992021@gmail.com>
2024-09-04 18:32:04 +0300

    Create Login Dto , Interface & Repo

---
+++ Data Access Layer/Repositry/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Data_Access_Layer.Entity;
+using Data_Access_Layer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Repositry
+{
+    public class Repository<T> : IRepositry<T> where T : class
+    {
+        private readonly ShippingDataBase db;
+        private readonly  DbSet<T> dbSet;
+
+        public Repository(ShippingDataBase db)
+        {
+            this.db = db;
+            dbSet=db.Set<T>();
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+           return await dbSet.ToListAsync();
+        }
+        public async Task<T?> GetAsyncById(int id)
+        {
+            T? entity = await dbSet.FindAsync(id);
+            return entity;
+        }
+        public async Task CreateAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            
+            db.Entry(entity).State = EntityState.Modified;
+            
+            
+        }
+        public async Task DeleteAsync(int id)
+        {
+            T? entity = await dbSet.FindAsync(id);
+            dbSet.Remove(entity);
+            
+        }
+
+
+
+        public async Task SaveAsync()
+        {
+            await db.SaveChangesAsync();
+        }
+
+    }
+}
