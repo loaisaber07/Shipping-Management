49207669235c6235947e2ca45398b53a7a461cf5
Ahmed Kamel <ahmedkamel19992021@gmail.com>
2024-09-04 22:22:08 +0300

    Dto & Repo

--- Data Access Layer/Interfaces/IUser.cs
+++ Data Access Layer/Interfaces/IUser.cs
@@ -1,4 +1,5 @@
-﻿using Data_Access_Layer.Repositry;
+﻿using Data_Access_Layer.Entity;
+using Data_Access_Layer.Repositry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace Data_Access_Layer.Interfaces
 {
     public interface IUser:IRepositry<User>
     {
+        Task<ApplicationUser?> GetUserAsyncById(string id);
+        Task DeleteUserAsync(string id);
 
     }
 }
