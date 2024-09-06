b114877bd4b7710a08d5ce6312360f3e473b7b0b
loaisaber07 <loai.sabir@outlook.com>
2024-09-06 23:08:07 +0300

    add govern, seller and city tables

---
+++ Data Access Layer/Entity/City.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Entity
+{
+    public  class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public int NormalCharge { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int PickUpCharge { get; set; }
+
+        public int? SpecialChargeForSeller { get; set; }
+    }
+}
---
+++ Data Access Layer/Entity/Seller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Entity
+{
+    public  class Seller :IdentityUser
+    { 
+
+        [Required]
+        public string StoreName { get; set; }
+        public int PickUp { get; set; } = 0;
+        public int? ValueOfRejectedOrder { get; set; } 
+    }
+}
