12b433bb65d91b9d29b39fd16b3dd15c6ba7d320
Ahmed Kamel <ahmedkamel19992021@gmail.com>
2024-09-13 11:34:49 +0300

    City Controller Done

---
+++ Business Layer/Services/CityService.cs
@@ -0,0 +1,73 @@
+﻿using Data_Access_Layer.DTO;
+using Data_Access_Layer.DTO.GovernAndCity;
+using Data_Access_Layer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.Services
+{
+    public class CityService
+    {
+        public static  City EditCity(EditCityDTO dto)
+        {
+            City city = new City()
+            {
+                ID=dto.Id,
+                Name = dto.Name,
+                NormalCharge = dto.NormalCharge,
+                PickUpCharge = dto.PickUpCharge,
+                SpecialChargeForSeller = dto.SpecialChargeForSeller,
+                GovernID=dto.GovernID
+            };
+            return city;
+        }
+
+        public static IEnumerable<GetCityDTO> MappingCity(IEnumerable<City> cityList)
+        {
+            List<GetCityDTO> list = new List<GetCityDTO>();
+            foreach (City city in cityList)
+            {
+                GetCityDTO dto = new GetCityDTO
+                {
+                    Name = city.Name,
+                    ID = city.ID,
+                    NormalCharge = city.NormalCharge,
+                    PickUpCharge=city.PickUpCharge,
+                     SpecialChargeForSeller=city.SpecialChargeForSeller,
+                     GovernID=city.GovernID
+                };
+                list.Add(dto);
+            }
+            return list;
+
+        }
+        public static City AddCity(AddCityDTO addCity)
+        {
+            City city = new City
+            {
+                Name=addCity.Name,
+                GovernID=addCity.GovernID,
+                NormalCharge=addCity.NormalCharge,
+                PickUpCharge = addCity.PickUpCharge,
+                SpecialChargeForSeller=addCity.SpecialChargeForSeller
+            };
+            return city;
+        }
+        public static EditCityDTO Mapping(City city)
+        {
+            EditCityDTO cityDTO = new EditCityDTO
+            {
+                Name = city.Name,
+                GovernID = city.GovernID,
+                NormalCharge = city.NormalCharge,
+                PickUpCharge = city.PickUpCharge,
+                Id = city.ID,
+                SpecialChargeForSeller = city.SpecialChargeForSeller
+            };
+            return cityDTO;
+        }
+    }
+}
--- Data Access Layer/Repositry/CityRepository.cs
+++ Data Access Layer/Repositry/CityRepository.cs
@@ -42,5 +42,35 @@ namespace Data_Access_Layer.Repositry
         {
     return await  dataBase.Cities.Where(s => s.GovernID == governID).ToListAsync();
         }
+
+        public async Task<string> GetNameById(int Id)
+        {
+            City? city = await dataBase.Cities.AsNoTracking().FirstOrDefaultAsync(s => s.ID == Id);
+            if (city is not null)
+            {
+                return city.Name;
+            }
+            return string.Empty;
+        }
+
+        public async Task<bool> IsExistById(int id)
+        {
+           City? city= await dataBase.Cities.AsNoTracking().FirstOrDefaultAsync(s => s.ID == id);
+            if (city == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<bool> IsExistByName(string Name)
+        {
+            City? city = await dataBase.Cities.FirstOrDefaultAsync(c => c.Name == Name);
+            if (city == null)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
