4c7b548478ebf3d70cd7686f9001971d126ead53
Ahmed Kamel <ahmedkamel19992021@gmail.com>
2024-09-18 17:13:25 +0300

    add delee seller and employee and get all seller

---
+++ Shippping Managment/Controllers/SellerController.cs
@@ -0,0 +1,53 @@
+﻿using Business_Layer.Services.Seller;
+using Data_Access_Layer.DTO.Seller;
+using Data_Access_Layer.Entity;
+using Data_Access_Layer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shippping_Managment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        private readonly IUser userRepo;
+
+        public SellerController( IUser userRepo)
+        {
+            this.userRepo = userRepo;
+        }
+        [HttpDelete]
+        public async Task<ActionResult> Delete(string sellerId)
+        {
+           bool chick = await userRepo.DeleteSellerAsync(sellerId);
+            if (!chick)
+            {
+                return BadRequest(new { Message = "Can not Delete Seller" });
+            }
+            await userRepo.SaveAsync();
+            return Ok(new {Message="Seller Deleted"});
+
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetSellerById(string sellerId)
+        {
+           Seller? seller = await userRepo.GetSellerAsyncById(sellerId);
+            if (seller is  null)
+            {
+                return NotFound(new {Message="Can not Find Seller Withe Same Id"});
+            }
+            GetSellerDTO getSellerDTO = SellerService.GetSellerDTO(seller); 
+            return Ok(getSellerDTO);
+ 
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<ActionResult>GetAllSellers()
+        {
+           IEnumerable<Seller> sellerList= await userRepo.GetAllSellers();
+            IEnumerable<GetSellerDTO> get = SellerService.GetAllSellers(sellerList);
+            return Ok(get);
+        }
+    }
+}
