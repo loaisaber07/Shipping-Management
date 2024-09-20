abfff45ce166958132a60ea746a05063ba769713
Ahmed Kamel <ahmedkamel19992021@gmail.com>
2024-09-20 16:54:20 +0300

    TypeOfReceiptDone

---
+++ Data Access Layer/DTO/TypeOfReceipt/EditTypeOfReceiptDTO.cs
@@ -0,0 +1,16 @@
+﻿using Data_Access_Layer.Custom_Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.DTO.TypeOfReceipt
+{
+    public class EditTypeOfReceiptDTO
+    {
+        public int ID { get; set; }
+        [UniqueTypeOfReceiptNameEdit]
+        public string Name { get; set; }
+    }
+}
